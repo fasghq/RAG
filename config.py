0bbf575247aeb50a090d59064914bc892eb4c9c5
fasghq <62699960+fasghq@users.noreply.github.com>
2026-05-16 17:04:11 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+OPENAI_API_KEY = ""
+
+AGENTS_FILE_GLOB = "agents.json"
+
+AGENTS_DIR_GLOB = "agents"
+
+FILES_DIR_GLOB = "agents/{agent_id}/files"
+
+DOCUMENTS_DIR_GLOB = "agents/{agent_id}/documents"
+
+CHUNKS_DIR_GLOB = "agents/{agent_id}/chunks"
+
+VECTORS_DIR_GLOB = "agents/{agent_id}/vectors"
